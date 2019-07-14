32d13192d8864b0203993305e9a62c13e9a66382
enver1323 <enver1323@gmail.com>
2019-07-14 16:55:23 +0500

    Fix migrations

--- database/migrations/2019_07_10_183121_create_slides_table.php
+++ database/migrations/2019_07_10_183121_create_slides_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSliderTable extends Migration
+class CreateSlidesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateSliderTable extends Migration
      */
     public function up()
     {
-        Schema::create('slider', function (Blueprint $table) {
+        Schema::create('slides', function (Blueprint $table) {
             /*
              * Columns
              */
