7a7914a1d36e26fcb0f8f97d10d45b56ecac4e98
Enver Menadjiev <enver1323@gmail.com>
2019-12-07 16:56:54 +0500

    Translate to Russian

---
+++ resources/lang/ru/frontend.php
@@ -0,0 +1,28 @@
+<?php
+return [
+    'home' => 'Главная',
+    'brands' => 'Бренды',
+    'articles' => 'Новости',
+    'contacts' => 'Контакты',
+    'readMore' => 'Подробнее',
+    'categories' => 'Категории',
+    'description' => 'Описание',
+    'topProducts' => 'Топ Товары',
+
+    'all' => 'Все',
+    'author' => 'Автор',
+    'product' => 'Товар',
+    'article' => 'Новость',
+
+    'name' => 'Имя',
+
+    'browseCategories' => 'Browse Featured Categories',
+
+    'related' => 'Опции',
+    'gallery' => 'Галерея',
+    'location' => 'Местоположение',
+    'overview' => 'Общая информация',
+    'techData' => 'Характеристики',
+    'technology' => 'Технологии',
+    'keepInTouch' => 'Контакты',
+];
