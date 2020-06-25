5e34297847e27fe3034fbe99ab787642be588160
Enver Menadjiev <enver1323@gmail.com>
2020-06-25 19:37:02 +0500

    Fix index Articles

--- public/build/js/productListSidebar.js
+++ public/build/js/productListSidebar.js
@@ -1,1 +1,151 @@
-!function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="/",n(n.s=6)}({6:function(e,t,n){e.exports=n("noJE")},noJE:function(e,t){var n=document.getElementsByClassName("job"),r=(document.getElementsByClassName("job-detail-button"),document.getElementById("sidebar")),o=function(e){var t=e.target.getAttribute("data-id");r.style.display="block",function(e){var t=document.getElementById("product-".concat(e,"-image"));t&&(document.getElementById("product-image").src=t.src)}(t),function(e){var t=document.getElementById("product-".concat(e,"-description"));t&&(document.getElementById("product-description").innerHTML=t.innerHTML)}(t),function(e){var t=document.getElementById("product-link"),n=document.getElementById("product-".concat(e,"-image"));n&&(t.href=n.parentNode.href,t.style.display="block")}(t),document.getElementById("sidebar").style.display="block !important"};for(var u=0;u<n.length;u++)n[u].addEventListener("mouseover",o,!1)}});
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 6);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./resources/js/models/productListSidebar.js":
+/*!***************************************************!*\
+  !*** ./resources/js/models/productListSidebar.js ***!
+  \***************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+var jobItems = document.getElementsByClassName("job");
+var jobItemButtons = document.getElementsByClassName("job-detail-button");
+var sidebar = document.getElementById('sidebar');
+
+var showJobDetails = function showJobDetails(event) {
+  var id = event.target.getAttribute('data-id');
+  sidebar.style.display = 'block';
+  setImage(id);
+  setDescription(id);
+  setReadMoreLink(id);
+  showSidebar();
+};
+
+function showSidebar() {
+  document.getElementById('sidebar').style.display = 'block !important';
+}
+
+function setDescription(id) {
+  var desc = document.getElementById("product-".concat(id, "-description"));
+  if (desc) document.getElementById('product-description').innerHTML = desc.innerHTML;
+}
+
+function setImage(id) {
+  var image = document.getElementById("product-".concat(id, "-image"));
+  if (image) document.getElementById('product-image').src = image.src;
+}
+
+function setReadMoreLink(id) {
+  var readMore = document.getElementById('product-link');
+  var link = document.getElementById("product-".concat(id, "-image"));
+
+  if (link) {
+    readMore.href = link.parentNode.href;
+    readMore.style.display = 'block';
+  }
+}
+
+for (var i = 0; i < jobItems.length; i++) {
+  jobItems[i].addEventListener('mouseover', showJobDetails, false);
+}
+
+/***/ }),
+
+/***/ 6:
+/*!*********************************************************!*\
+  !*** multi ./resources/js/models/productListSidebar.js ***!
+  \*********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! D:\Projects\php\printing_s\resources\js\models\productListSidebar.js */"./resources/js/models/productListSidebar.js");
+
+
+/***/ })
+
+/******/ });
